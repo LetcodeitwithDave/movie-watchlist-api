d100397a4722e97e850959e2f5476fea41fd8b7c
David <sololadavid4@gmail.com>
2026-04-25 07:37:26 +0100

    fixed prismaClient issue and server not running by installing and setup prisma adapter-pg, required for prisma version 7.XX

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 
 const prisma = new PrismaClient({
   log:
     process.env.NODE_ENV === "development"
       ? ["query", "error", "warn"]
       : ["error"],
+  adapter, //prisma7 adapter required for postgres connection
 });
 
 const connectDB = async () => {
