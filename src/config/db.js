9870d0f99ef4fc10c832405900ee5c53bd89cee5
David <sololadavid4@gmail.com>
2026-04-27 20:16:11 +0100

    fixed problem with 'Invalid  invocation:' when registering a new user, forgot import dotenv

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
+
+// console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 // console log log data
 const logData =
