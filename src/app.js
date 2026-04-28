b5e1e2c01b2b6a77484438f05d80d3d455a37ebd
David <sololadavid4@gmail.com>
2026-04-28 21:48:55 +0100

    Protected route

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import authRoute from "./routes/authRoute.js";
+import { getCurrenUser } from "./routes/getCurrentUser.js";
+import { protectRoute } from "./middleware/authMiddleware.js";
 
 const app = express();
 
@@ -8,5 +10,6 @@ app.use(express.urlencoded({ extended: false }));
 
 // ROUTES
 app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/currentuser", protectRoute, getCurrenUser);
 
 export default app;
