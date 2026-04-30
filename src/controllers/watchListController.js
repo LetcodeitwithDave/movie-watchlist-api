b8b1a10f6943bd5ecaf4875729f1fcdec91893e8
David <sololadavid4@gmail.com>
2026-05-01 00:46:31 +0100

    watchlist controller logic

---
+++ src/controllers/watchListController.js
@@ -0,0 +1,11 @@
+import { prisma } from "../config/db.js";
+
+const addToWatchList = async (req, res) => {
+  // const {}
+};
+
+const getWatchList = async (req, res) => {};
+
+const removeFromWatchList = async (req, res) => {};
+
+export { addToWatchList, getWatchList, removeFromWatchList };
