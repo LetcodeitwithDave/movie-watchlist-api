1f42d17434cb193502d925e7e464e105d01ce0f0
David <sololadavid4@gmail.com>
2026-05-01 18:48:23 +0100

    add movies to watchlist logic

--- src/controllers/watchListController.js
+++ src/controllers/watchListController.js
@@ -1,7 +1,71 @@
 import { prisma } from "../config/db.js";
 
 const addToWatchList = async (req, res) => {
-  // const {}
+  try {
+    const { movieId, status, rating, notes } = req.body;
+
+    const userId = req.user.id;
+
+    console.log("userId name", req.user.name);
+
+    // validate input
+    if (!movieId) {
+      return res.status(400).json({
+        status: "error",
+        message: "movieId is required",
+      });
+    }
+
+    const movieExists = await prisma.movie.findUnique({
+      where: {
+        id: movieId,
+      },
+    });
+
+    if (!movieExists) {
+      return res.status(404).json({
+        status: "error",
+        message: "Movie not Found",
+      });
+    }
+
+    const existingWatchlistItem = await prisma.watchlistItem.findFirst({
+      where: {
+        userId,
+        movieId,
+      },
+    });
+
+    if (existingWatchlistItem) {
+      return res.status(409).json({
+        status: "error",
+        message: "Movie already in watchlist",
+      });
+    }
+
+    // add to watchlist
+    const watchlistItem = await prisma.watchlistItem.create({
+      data: {
+        userId,
+        movieId,
+        status: status || "PLANNED",
+        rating,
+        notes,
+      },
+    });
+
+    return res.status(201).json({
+      status: "success",
+      data: watchlistItem,
+    });
+  } catch (error) {
+    console.error("Add to watchlist error:", error);
+
+    return res.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
 };
 
 const getWatchList = async (req, res) => {};
