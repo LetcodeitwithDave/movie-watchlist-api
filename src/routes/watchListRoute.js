63c9d78d6e19807be282b4abd59d36118fe568d9
David <sololadavid4@gmail.com>
2026-05-03 22:41:36 +0100

    Delete logged in user movie in watchlist

--- src/routes/watchListRoute.js
+++ src/routes/watchListRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   addToWatchList,
   getWatchList,
+  removeFromWatchList,
 } from "../controllers/watchListController.js";
 
 import { protectRoute } from "../middleware/authMiddleware.js";
@@ -12,5 +13,6 @@ router.use(protectRoute);
 
 router.post("/", addToWatchList);
 router.get("/", getWatchList);
+router.delete("/:movieId", removeFromWatchList);
 
 export default router;
