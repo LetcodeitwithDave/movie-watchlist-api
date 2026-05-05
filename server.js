4aef19e8d22460d9ffda67989269be6331a02fa7
David <sololadavid4@gmail.com>
2026-05-05 20:28:20 +0100

    preparing file for deployments

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ import app from "./src/app.js";
 connectDB(); // connect to database
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
