c422ca4415da476b562b53c199233bb66ead9e80
David <sololadavid4@gmail.com>
2026-05-05 18:34:06 +0100

    .

--- src/config/db.js
+++ src/config/db.js
@@ -6,8 +6,6 @@ const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
 
-// console.log("DATABASE_URL:", process.env.DATABASE_URL);
-
 // console log log data
 const logData =
   process.env.NODE_ENV === "development"
